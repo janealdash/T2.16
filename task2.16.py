52936ffa10cc69ffa6ca19812afe4231023e2c64
janealdash <janarka.k@gmail.com>
2019-11-25 20:00:26 +0600

    janara

---
+++ task2.16.py
@@ -0,0 +1,14 @@
+# If you were on the moon now, your weight will be 16,5% of your earth weight.
+# To calculate it you have to multiple to 0,165. If next 15 years your weight will
+# increase 1 kg each year. What will be your weight each year on the moon next
+# 15 years?
+
+for weight in range (50, 58): 
+    print (weight)
+
+
+
+    # for - sikl 
+
+
+    # posle printa argument always v skobkah
